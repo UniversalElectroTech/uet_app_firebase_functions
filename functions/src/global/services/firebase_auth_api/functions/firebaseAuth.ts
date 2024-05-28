fd270c35a5de15b04c51d3ec58877f06bc60ab92
Jourdan Tears <Jourdan.Tears@uet.local>
2024-05-28 16:51:10 +0800

    Fixed createUserFromInvite function name

--- functions/src/global/services/firebase_auth_api/functions/firebaseAuth.ts
+++ functions/src/global/services/firebase_auth_api/functions/firebaseAuth.ts
@@ -2,7 +2,7 @@ import { CallableRequest, onCall } from "firebase-functions/v2/https";
 import { createUserFromInvite } from "./createUserFromInviteFunction";
 
 // Returns all RCD testing complete jobs from the SimproAPI
-exports.addEmployeeToApp = onCall(
+exports.createUserFromInvite = onCall(
 	{ timeoutSeconds: 10, maxInstances: 1 },
 	async (request: CallableRequest) => {
 		return await createUserFromInvite(request);
